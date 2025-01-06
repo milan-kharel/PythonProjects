0bfc2998cdef190060991bbd455e6271447cf8d9
milan-kharel <milan.kharel120@gmail.com>
2025-01-06 22:37:48 +0545

    writing logics and imported hangman stages

--- hangman.py
+++ hangman.py
@@ -5,3 +5,117 @@ def get_word():
     word = random.choice(word_list)
     return word.upper()
 
+def play(word):
+
+    word_completion = "_"*len(word)
+    guessed = False
+    guessed_letters = []
+    guessed_words = []
+    tries = 6
+    print("Let's play hangman")
+    print(display_hangman(tries))
+    print(word_completion)
+    print("\n")
+
+    while not guessed and tries > 0:
+        guess = input("please guess a lettr:").upper()
+        if len(guess) == 1 and guess.isalpha(): #check if input is a sigle letter
+            if guess in guessed_letters:
+                print("U already guessed the letter",guess)
+            elif guess not in word:
+                print(guess, "is not in the word")
+                tries -= 1
+                guessed_letters.append(guess)
+            else:
+                print("good job", guess, "is correct word")
+                guessed_letters.append(guess)
+                
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def display_hangman(tries):
+    stages = [  # final state: head, torso, both arms, and both legs
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / \\
+                   -
+                """,
+                # head, torso, both arms, and one leg
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |     / 
+                   -
+                """,
+                # head, torso, and both arms
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|/
+                   |      |
+                   |      
+                   -
+                """,
+                  # head, torso, and one arm
+                """
+                   --------
+                   |      |
+                   |      O
+                   |     \\|
+                   |      |
+                   |     
+                   -
+                """,
+                # head and torso
+                """
+                   --------
+                   |      |
+                   |      O
+                   |      |
+                   |      |
+                   |     
+                   -
+                """,
+                # head
+                """
+                   --------
+                   |      |
+                   |      O
+                   |    
+                   |      
+                   |     
+                   -
+                """,
+                # initial empty state
+                """
+                     --------
+                   |      |
+                   |      
+                   |    
+                   |      
+                   |     
+                   -
+                """
+    ]
+    return stages[tries]
+
+
