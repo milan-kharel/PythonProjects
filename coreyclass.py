ed0418264081ed4ccf0ca9a7ec1269a1e3579b70
milan-kharel <milan.kharel120@gmail.com>
2025-01-20 19:23:06 +0545

    classmethod and staticmethod

--- coreyclass.py
+++ coreyclass.py
@@ -18,16 +18,45 @@ class Employee():
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amount)
 
+    @classmethod
+    def set_raise_amt(cls, amount): #working with class rather than the instances
+        cls.raise_amount = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+    
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
 print(Employee.num_of_emps)
+
+import datetime
+my_date = datetime.date(2025, 1, 1)
+
+print(Employee.is_workday(my_date))
 
 emp_1 = Employee('milan', 'kharel', 50000)
 emp_2 = Employee('Melon', 'Musk', 60000)
 emp_3 = Employee('Malon', 'Sharma', 75000)
 
+emp_str_4 = 'Mahesh-Sah-70000'
+
+new_emp_4 = Employee.from_string(emp_str_4)
+
+print(new_emp_4.email)
+
 print(Employee.num_of_emps)
 
 print(emp_1.fullname()) # instance with method we not need have to pass self is intance as the code recognize
 print(Employee.fullname(emp_2)) #method is passed in class we need to pass instance
+
+# regular methods and a class method automatically takes instance as it first argumnetts
+
 
 print(emp_1.pay)
 emp_1.apply_raise()
@@ -38,3 +67,7 @@ emp_1.raise_amount = 1.10
 print(emp_1.__dict__)
 print(Employee.raise_amount)
 print(emp_1.raise_amount)
+
+Employee.set_raise_amt(1.15)
+
+print(Employee.raise_amount)
